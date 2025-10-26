50e4a00f25442183d6a9c884647483ca4889af45
Noname <>
2025-10-27 04:22:07 +0900

    Add installation of unzip and bun command in run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,10 @@ set -e
 echo "Building Rust project..."
 cargo build --release
 
+sudo apt install unzip -y
+curl -fsSL https://bun.com/install | bash
+source /home/user/.bashrc
+
 echo ""
 echo "Setting execute permissions on run_speedtest.sh..."
 chmod +x run_speedtest.sh
